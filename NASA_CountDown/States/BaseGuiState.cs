a84e12843ec0f30b98aceab27874616118901df0
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-07-30 07:10:39 -0400

    Merged in someupdates

--- NASA_CountDown/States/BaseGuiState.cs
+++ NASA_CountDown/States/BaseGuiState.cs
@@ -1,6 +1,12 @@
 ﻿using NASA_CountDown.Common;
 using NASA_CountDown.StateMachine;
 
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace NASA_CountDown.States
 {
     public abstract class BaseGuiState: KFSMState, IGuiBehavior
